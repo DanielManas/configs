08fbde5f0ccaf3ca265c7f954d847d9c1f015bf0
Daniel Mañas <53346563+dmanas0@users.noreply.github.com>
2020-02-22 00:21:21 +0100

    Update routing.sh

--- routing.sh
+++ routing.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
+### Careful: if you use iptables for other configurations, using the stop 
+### option of this script will mess your iptables configuration.
 
-iptables -t nat -A POSTROUTING --out-interface enp0s3 -j MASQUERADE
-iptables -A FORWARD --in-interface enp0s8 -j ACCEPT
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-#case "$1" in
-#  start)
-#    echo 1 > /proc/sys/net/ipv4/ip_forward
-#    ;;
-#  stop)
-#    echo 0 > /proc/sys/net/ipv4/ip_forward
-#    ;;
-#  *)
-#    echo "Usage: /etc/init.d/routing.sh {start|stop}"
-#    exit 1
-#    ;;
-#esac
-
+case "$1" in
+  start)
+    iptables -t nat -A POSTROUTING --out-interface enp0s3 -j MASQUERADE
+    iptables -A FORWARD --in-interface enp0s8 -j ACCEPT
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+    ;;
+  stop)
+    iptables -F
+    iptables -Z
+    iptables -t nat -F
+    echo 0 > /proc/sys/net/ipv4/ip_forward
+    ;;
+  *)
+    echo "Usage: /etc/init.d/routing.sh {start|stop}"
+    exit 1
+    ;;
+esac
 exit 0
